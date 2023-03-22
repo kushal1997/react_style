490cb37908b2db057cff22d448a2608404a5b1a4
kushal1997 <kushalrao103@gmail.com>
2023-03-22 21:59:51 +0530

    global level styling

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -5,7 +5,7 @@ export const TaskCard = ({task,handleDelete,info}) => {
     <>
         <li className={ task.completed? "completed":"incomplete"}>
               <span>{ task.id} - { task.name} - {info}</span>
-              <button onClick={()=> handleDelete( task.id)}>Delete</button>
+              <button onClick={()=> handleDelete( task.id) } className="delete">Delete</button>
             </li>
     </>
   )
