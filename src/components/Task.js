176287fa56dc4575f7cdfd3ab7ad37e6bf1edb38
kushal1997 <kushalrao103@gmail.com>
2023-03-23 08:15:31 +0530

    dynamic inline level stylling

--- src/components/Task.js
+++ src/components/Task.js
@@ -12,45 +12,50 @@ export const Task = ({ info }) => {
   const [show, setShow] = useState(true);
   const styles = {
     color: "red",
-    border: "1px solid red",
+    border: "2px solid",
+        // ======================
+        // DYNAMIC INLINE LEVEL STYLING
+        // ======================
+    
+    borderColor: show? "green":"red",
     padding: "20px"
-  }
+}
 
-  function handleDelete(id) {
-    setTasks(tasks.filter(task => id !== task.id));
+function handleDelete(id) {
+  setTasks(tasks.filter(task => id !== task.id));
 
-  }
+}
 
-  return (
-    <section className="taskList">
-      {/* ======================
-          INLINE LEVEL STYLING
-          ====================== */}
-      <h1 style={
-        styles
-      }>Task List</h1>
-      <ul>
-        <button onClick={() => setShow(!show)}>Toggle</button>
-        {
-          show && tasks.map((task) => (
-            <TaskCard info={info} key={task.id} task={task} handleDelete={handleDelete} />
-          ))
-        }
+return (
+  <section className="taskList">
+    {/* ======================
+    INLINE LEVEL STYLING
+    ====================== */}
+    <h1 style={
+      styles
+    }>Task List</h1>
+    <ul>
+      <button onClick={() => setShow(!show)}>{show?"Hide":"Show"}</button>
+      {
+        show && tasks.map((task) => (
+          <TaskCard info={info} key={task.id} task={task} handleDelete={handleDelete} />
+        ))
+      }
 
-      </ul>
-      <BoxCard result="success">
-        <p className="title">Lorem ipsum dolor sit amet.</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, fugiat?</p>
-      </BoxCard>
-      <BoxCard result="warning">
-        <p className="title">Lorem ipsum dolor sit.</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatibus fuga aliquam, beatae officia possimus.</p>
-      </BoxCard>
-      <BoxCard result="alert">
-        <p className="title">Lorem, ipsum dolor.</p>
-        <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum omnis atque quos neque et. Dignissimos quae itaque voluptatibus doloribus vero?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, natus repellat illum repudiandae rem quia dignissimos ut dolore consectetur provident eaque ducimus cupiditate nemo numquam?</p>
-      </BoxCard>
-    </section>
-  )
+    </ul>
+    <BoxCard result="success">
+      <p className="title">Lorem ipsum dolor sit amet.</p>
+      <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, fugiat?</p>
+    </BoxCard>
+    <BoxCard result="warning">
+      <p className="title">Lorem ipsum dolor sit.</p>
+      <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatibus fuga aliquam, beatae officia possimus.</p>
+    </BoxCard>
+    <BoxCard result="alert">
+      <p className="title">Lorem, ipsum dolor.</p>
+      <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum omnis atque quos neque et. Dignissimos quae itaque voluptatibus doloribus vero?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, natus repellat illum repudiandae rem quia dignissimos ut dolore consectetur provident eaque ducimus cupiditate nemo numquam?</p>
+    </BoxCard>
+  </section>
+)
 }
