5376328cf007817b2c590e6b2da9bb70802d00ec
kushal1997 <kushalrao103@gmail.com>
2023-03-23 08:25:38 +0530

    Module level styling

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,10 +1,12 @@
 import "./taskCard.css"
+import styles from "./taskCard.module.css"
+
 export const TaskCard = ({task,handleDelete,info}) => {
     
   return (
     <div className="taskcard ">
         <li className={`${task.completed? "completed":"incomplete"}` }>
-              <span>{ task.id} - { task.name} - {info}</span>
+              <span className={styles.title}>{ task.id} - { task.name} - {info}</span>
               <button onClick={()=> handleDelete( task.id) } className="delete">Delete</button>
             </li>
     </div>
